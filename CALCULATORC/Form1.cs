0eb13ed496492dfbb77b19aa2f7d3f87705dc035
unclave <phantom-lss@mail.ru>
2017-07-10 15:19:50 +0600

    initial project version

--- CALCULATORC/Form1.cs
+++ CALCULATORC/Form1.cs
@@ -16,5 +16,25 @@ namespace CALCULATORC
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
